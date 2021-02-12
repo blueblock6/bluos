21ffd103609a2ab20164eb0d955ba1ed3ca0fc80
blueblock6 <66493630+blueblock6@users.noreply.github.com>
2021-02-12 13:42:39 -0500

    Update boot.lua

--- bluos/boot.lua
+++ bluos/boot.lua
@@ -19,7 +19,7 @@ end
 API.heading("BluOS Booting")
 computer.beep()
 os.sleep(1)
-term.heading("Installing finishinstall.lua")
+API.heading("Installing finishinstall.lua")
 os.execute("wget -f https://github.com/blueblock6/bluos/raw/master/bluos/finishinstall.lua /bluos/finishinstall.lua")
 
 os.execute("/bluos/finishinstall.lua")
