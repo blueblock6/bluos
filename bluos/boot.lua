5176fbbd195baa31d5dc48f57addfb61b2cfae69
blueblock6 <66493630+blueblock6@users.noreply.github.com>
2021-02-12 14:49:03 -0500

    More and More Checks

--- bluos/boot.lua
+++ bluos/boot.lua
@@ -19,7 +19,7 @@ end
 API.heading("BluOS Booting")
 computer.beep()
 os.sleep(1)
-if filesystem.exists("/bluos/finishinstall.lua") then
+if filesystem.exists("/bluos/finishinstall.lua") == true then
   API.heading("Installing finishinstall.lua")
   os.execute("wget -f https://github.com/blueblock6/bluos/raw/master/bluos/finishinstall.lua /bluos/finishinstall.lua")
 end
